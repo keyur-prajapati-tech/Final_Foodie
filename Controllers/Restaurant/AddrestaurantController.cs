3ac79245c43acd82c2494a82130b2dd99d7c4bf0
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-04-08 14:01:21 +0530

    rs commit

--- Controllers/Restaurant/AddrestaurantController.cs
+++ Controllers/Restaurant/AddrestaurantController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Foodie.Models.Restaurant;
+using Foodie.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Foodie.Controllers.Restaurant
 {
     [Route("AddRestaurant")]
     public class AddrestaurantController : Controller
     {
+        private readonly IRestaurantRepository _repository;
+
+        public AddrestaurantController(IRestaurantRepository repository)
+        {
+            _repository = repository;
+        }
+
         [Route("Registerres")]
         public IActionResult Registerres()
         {
@@ -15,6 +24,41 @@ namespace Foodie.Controllers.Restaurant
         public IActionResult ResDocument()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterRestaurnt(RestaurantRegistrationViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var owner = new tbl_owner_details
+                {
+                    owner_name = model.tbl_Owner.owner_name,
+                    owner_email = model.tbl_Owner.owner_email,
+                    owner_contact = model.tbl_Owner.owner_contact
+                };
+
+                int ownerid = _repository.AddOwner(owner);
+
+                var restaurant = new tbl_restaurant
+                {
+                    restaurant_name = model.tbl_Restaurant.restaurant_name,
+                    restaurant_contact = model.tbl_Owner.owner_contact,
+                    restaurant_email = model.tbl_Owner.owner_email,
+                    restaurant_street = model.tbl_Restaurant.restaurant_street,
+                    restaurant_pincode = model.tbl_Restaurant.restaurant_pincode,
+                    restaurant_lat = "dfg",
+                    restaurant_lag = "ddgbfg",
+                    restaurant_isActive = true,
+                    est_id = 1,
+                    owner_id = model.tbl_Owner.owner_id
+                };
+
+                _repository.AddRestaurant(restaurant);
+
+                return RedirectToAction("MenuItems");
+            }
+            return View(model);
         }
 
         [Route("MenuItems")]
