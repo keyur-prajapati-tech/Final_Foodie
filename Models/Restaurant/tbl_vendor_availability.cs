7b9e72c84702881f1d37243cfc11abc8dbd02809
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-04-15 17:57:07 +0530

    rs

--- Models/Restaurant/tbl_vendor_availability.cs
+++ Models/Restaurant/tbl_vendor_availability.cs
@@ -6,9 +6,7 @@
         public int Restaurant_id { get; set; }
         public DateTime open_time { get; set; }
         public DateTime close_time { get; set; }
-
         public string day_of_week { get; set; }
-
         public bool is_Open { get; set; }
 
     }
