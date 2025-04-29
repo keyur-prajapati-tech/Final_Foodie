4430d9ec0c74372775009dc5b31367f4da37941b
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-04-29 17:42:13 +0530

    rs

---
+++ Models/Restaurant/OutletInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Foodie.Models.Restaurant
+{
+    public class OutletInfo
+    {
+        public int restaurant_id { get; set; }
+        public string restaurant_name { get; set; }
+        public string restaurant_address { get; set; }
+        public string restaurant_pincode { get; set; }
+        public string restaurant_phone { get; set; }
+        public string restaurant_email { get; set; }
+      
+        public string restaurant_opening_hours { get; set; }
+        public string restaurant_closing_hours { get; set; }
+
+        public bool restaurant_isOnline { get; set; }
+
+
+    }
+}
